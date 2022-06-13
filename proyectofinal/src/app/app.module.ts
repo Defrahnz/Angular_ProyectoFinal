e4630d105b35b6488fb996a3955baa4e267daa9b
Defrahnz <carlos.ibarra@maindsoft.net>
2022-06-12 23:15:28 -0500

    Se crearon los componentes necesarios para la app

--- proyectofinal/src/app/app.module.ts
+++ proyectofinal/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListEmpComponent } from './components/list-emp/list-emp.component';
+import { CreateEmpComponent } from './components/create-emp/create-emp.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListEmpComponent,
+    CreateEmpComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
