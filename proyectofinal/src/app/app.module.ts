81058e19321e25f77f5ea0056bf099f5b77b57c9
Defrahnz <carlos.ibarra@maindsoft.net>
2022-06-15 20:14:09 -0500

    Se crearon las componentes necesarias para el navbar y el login

--- proyectofinal/src/app/app.module.ts
+++ proyectofinal/src/app/app.module.ts
@@ -3,15 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ListEmpComponent } from './components/list-emp/list-emp.component';
-import { CreateEmpComponent } from './components/create-emp/create-emp.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListEmpComponent,
-    CreateEmpComponent,
     NavbarComponent
   ],
   imports: [
--- proyectofinal/src/app/auth/login/login.component.spec.ts
+++ proyectofinal/src/app/auth/login/login.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CreateEmpComponent } from './create-emp.component';
+import { LoginComponent } from './login.component';
 
-describe('CreateEmpComponent', () => {
-  let component: CreateEmpComponent;
-  let fixture: ComponentFixture<CreateEmpComponent>;
+describe('LoginComponent', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CreateEmpComponent ]
+      declarations: [ LoginComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(CreateEmpComponent);
+    fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
