fee9febc76e8167c8fd13eef69b62134189b293c
Defrahnz <carlos.ibarra@maindsoft.net>
2022-06-15 20:28:10 -0500

    Se craron y modificaron los ficheros correspondientes para poder utilizar firebase en angular

--- proyectofinal/src/environments/environment.ts
+++ proyectofinal/src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'proyecto-final-angular-isc',
+    appId: '1:367483483595:web:4752386ebd06e1e8f0b24e',
+    storageBucket: 'proyecto-final-angular-isc.appspot.com',
+    apiKey: 'AIzaSyCurdk0bAw5UlP0OHD0duCwQGYDM07792o',
+    authDomain: 'proyecto-final-angular-isc.firebaseapp.com',
+    messagingSenderId: '367483483595',
+  },
   production: false
 };
 
