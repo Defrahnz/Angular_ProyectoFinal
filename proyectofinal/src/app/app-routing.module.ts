adb3add608391af94b922e689d166801afcf9cfc
Defrahnz <carlos.ibarra@maindsoft.net>
2022-06-17 23:13:49 -0500

    Se convirtio un cambio

--- proyectofinal/src/app/app-routing.module.ts
+++ proyectofinal/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { QrCodeModule } from 'ng-qrcode';
 
 const routes: Routes = [
-  {path:'/',redirectTo:'/home',pathMatch:'full'}
+  {path:'/',redirectTo:'/home',pathMatch:'full'},
   { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }, 
   { path: 'login', loadChildren: () => import('./auth/login/login.module').then(m => m.LoginModule) }, 
   { path: 'register', loadChildren: () => import('./auth/register/register.module').then(m => m.RegisterModule) }];
